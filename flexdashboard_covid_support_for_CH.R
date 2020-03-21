6651497bcbe7f7d8a5cd8afa3781b6fcc9b2a440
Kate H <KateMHansonPhD@gmail.com>
2020-03-21 14:18:52 +0000

    Created flexdashboard

---
+++ flexdashboard_covid_support_for_CH.R
@@ -0,0 +1,13 @@
+#NC COVID Community Resources
+#Built using flexdashboard + shiny
+
+#Required packages: 
+# - markdown
+# - flexdashboard
+# - tidyverse
+# - shiny
+# - leaflet
+# - googlesheets4
+
+
+rmarkdown::draft("dashboard.Rmd", template = "flex_dashboard", package = "flexdashboard")
