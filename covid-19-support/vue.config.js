5f7643f852fbf7cb9f4a4510902985d461f50e83
Chris Houston <chouston@vizioz.com>
2020-03-25 22:34:58 -0400

    Adding the public path for the website deployment.

--- covid-19-support/vue.config.js
+++ covid-19-support/vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/COVID-Support-For-CH/'
+    : '/',
+
     pluginOptions: {
         i18n: {
             locale: 'en',
