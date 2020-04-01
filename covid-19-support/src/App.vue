a2659b31045343eecaab2292a51ea9c5686c95b9
Chris Houston <chouston@vizioz.com>
2020-04-01 03:39:48 -0400

    A few small changes.

--- covid-19-support/src/App.vue
+++ covid-19-support/src/App.vue
@@ -92,7 +92,7 @@ export default {
         markers = this.entries.filter((c) => c.gsx$resource.$t === this.need && c.gsx$status.$t === 'active')
       }
 
-      const dayFilters = ['sun', 'mon', 'tues', 'wed', 'thr', 'fri', 'sat'].map((attr) => `gsx$${attr}`)
+      const dayFilters = ['mon', 'tues', 'wed', 'thr', 'fri', 'sat', 'sun'].map((attr) => `gsx$${attr}`)
       const dayFilter = dayFilters[this.day]
 
       return markers.filter((c) => c[dayFilter].$t !== '0')
