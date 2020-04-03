7effce30d88043323ee31b31fb4385ca8ab479d6
Chris Houston <chouston@vizioz.com>
2020-04-03 10:59:04 -0400

    Hopefully this will fix the custom domain issue :)

--- covid-19-support/vue.config.js
+++ covid-19-support/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/COVID-Support-For-CH/' : '/',
-
+  // publicPath: process.env.NODE_ENV === 'production' ? '/COVID-Support-For-CH/' : '/',
+  publicPath: '/',
   pluginOptions: {
     i18n: {
       locale: 'en',
