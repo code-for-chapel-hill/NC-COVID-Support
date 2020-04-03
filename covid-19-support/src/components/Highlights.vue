cfc8c4259e266514baf3f8089a14ef55e844b248
Chris Houston <chouston@vizioz.com>
2020-04-03 14:40:33 -0400

    Adding Google Analytics.

--- covid-19-support/src/components/Highlights.vue
+++ covid-19-support/src/components/Highlights.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="row highlights" v-if="valueBoxes[0] != null">
     <div class="col-6 col-md-3 order-md-1">
-      <value-box :icon="valueBoxes[0].icon" :title="valueBoxes[0].title" :value="valueBoxes[0].value" class="bg-blue" />
+      <value-box :content="valueBoxes[0]" class="bg-blue" />
     </div>
     <div class="col-6 col-md-3 order-md-2">
-      <value-box :icon="valueBoxes[1].icon" :title="valueBoxes[1].title" :value="valueBoxes[1].value" class="bg-green" />
+      <value-box :content="valueBoxes[1]" class="bg-green" />
     </div>
     <div class="col-6 col-md-3 order-md-4">
-      <value-box :icon="valueBoxes[2].icon" :title="valueBoxes[2].title" :value="valueBoxes[2].value" class="bg-green" />
+      <value-box :content="valueBoxes[2]" class="bg-green" />
     </div>
     <div class="col-6 col-md-3 order-md-3">
-      <value-box :icon="valueBoxes[3].icon" :title="valueBoxes[3].title" :value="valueBoxes[3].value" class="bg-blue" />
+      <value-box :content="valueBoxes[3]" class="bg-blue" />
     </div>
   </div>
 </template>
