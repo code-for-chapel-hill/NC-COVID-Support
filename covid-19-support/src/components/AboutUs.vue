98dc62e9dfdee0762c42d1e390d42979dc02be9c
Dane Summers <dsummersl@yahoo.com>
2020-04-17 21:00:45 -0400

    Ensure height is full screen height on mobile #116

--- covid-19-support/src/components/AboutUs.vue
+++ covid-19-support/src/components/AboutUs.vue
@@ -3,7 +3,7 @@
     <template v-slot:modal-title>
       {{ $t('about.title') }}
     </template>
-    <b-card no-body>
+    <b-card class="h-100" no-body>
       <b-tabs card>
         <b-tab :title="$t('about.getinvolved')">
           <p>{{ $t('about.devhelp.info') }}</p>
