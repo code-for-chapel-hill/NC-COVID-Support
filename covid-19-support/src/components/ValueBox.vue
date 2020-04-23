ca39e43ca4581032c1705cd16c876a2a7a9954f3
Chris Houston <chouston@vizioz.com>
2020-04-23 15:12:04 -0400

    Fixing the highlight box styles across all device sizes.

--- covid-19-support/src/components/ValueBox.vue
+++ covid-19-support/src/components/ValueBox.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="valuebox poppins" :class="{ selected: content.selected }" @click="$emit('box-selected', { need: content.need })">
     <div class="number">{{ content.value }}</div>
-    <div class="title" :class="{ten: content.value > 9, hundred: content.value > 99, thousand: content.value > 999}">{{ content.title }}</div>
+    <div class="title" :class="{ ten: content.value > 9, hundred: content.value > 99, thousand: content.value > 999 }">
+      {{ content.title }}
+    </div>
     <i class="fas" v-bind:class="content.icon" v-if="content.icon != ''"></i>
   </div>
 </template>
@@ -36,8 +38,8 @@ export default {
 }
 
 .number {
-    position: absolute;
-    left: 10px;
+  position: absolute;
+  left: 10px;
 }
 
 .number,
@@ -48,12 +50,12 @@ export default {
   height: 50px;
 }
 .title {
-    position: absolute;
-    line-height: 1rem;
-    margin-right: 24px;
-    font-size: 0.7rem;
-    z-index: 3;
-    left: 26px;
+  position: absolute;
+  line-height: 1rem;
+  margin-right: 24px;
+  font-size: 0.7rem;
+  z-index: 3;
+  left: 26px;
 }
 
 .title.ten {
@@ -77,7 +79,7 @@ export default {
     display: inline-block;
     position: absolute;
     top: 10px;
-    left:18px !important;
+    left: 18px !important;
   }
 }
 @media (min-width: 1200px) {
