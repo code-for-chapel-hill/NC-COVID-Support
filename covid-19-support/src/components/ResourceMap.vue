0f673a1395140b8d583e533a312467de226ef149
Chris Houston <chouston@vizioz.com>
2020-04-22 22:14:03 -0400

    Changing the main marker color to one of our brand colors.

--- covid-19-support/src/components/ResourceMap.vue
+++ covid-19-support/src/components/ResourceMap.vue
@@ -97,6 +97,7 @@ export default {
     latLng,
     selectedIcon(selected, item) {
       const isOpen = item.oc
+      let markerColor = isOpen ? '#566ca9' : '#999'
       const iconClasses = businessIcon(item.marker)
       if (selected) {
         markerColor = '#ff3d3d'
