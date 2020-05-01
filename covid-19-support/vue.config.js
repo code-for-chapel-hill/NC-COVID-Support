d9fc2228f59b22fc14fe6cf2d51784ce7671bb64
Chris Houston <chouston@vizioz.com>
2020-05-01 01:25:50 -0400

    Adding "themes" to allow third parties to brand the site with their own color schemes etc.

--- covid-19-support/vue.config.js
+++ covid-19-support/vue.config.js
@@ -8,5 +8,25 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: true
     }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+          // create alias for white label SCSS variable files
+          'vueAppTheme': "../themes/" + process.env.VUE_APP_THEME,
+      }
+    }
+  },
+  css: {
+    loaderOptions: {
+     sass: {
+      prependData: `
+       @import "./node_modules/bootstrap/scss/_functions.scss";
+       @import "~bootstrap/scss/variables";
+       @import "~bootstrap/scss/mixins";
+       @import './src/scss/Mixins.scss';
+       @import "./src/themes/${process.env.VUE_APP_THEME !== undefined ? process.env.VUE_APP_THEME : "NCCovidSupport"}/SCSS/custom.scss";
+      `}
+    }
   }
 }
