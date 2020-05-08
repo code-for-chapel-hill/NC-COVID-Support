d5a9c69a24d5808f8ed5cceed4ac7d5fa8008a91
Chris Houston <chouston@vizioz.com>
2020-05-08 01:46:14 -0400

    Fixed the sorting issue

--- covid-19-support/src/themes/AmericanDemoTheme/theme.config.js
+++ covid-19-support/src/themes/AmericanDemoTheme/theme.config.js
@@ -3,7 +3,14 @@ export const theme = {
     spreadsheetUrl: 'https://spreadsheets.google.com/feeds/list/1uBmMediCrxYl5qWqapbj4SOFTEXHoipE2oibMa2GTFo/1/public/values?alt=json'
   },
   maps: {
-    normal: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png',
-    dark: 'https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png'
+    normal: {
+      url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="https://carto.com/">Carto</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    },
+    dark: {
+      url: 'https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png', // 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+      attribution:
+        '&copy; <a href="https://carto.com/">Carto</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    }
   }
 }
